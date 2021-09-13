47d5713128ff63126589aac3b2f5957babc7a83d
Swapnil Nawale <swapnilnawale18@gmail.com>
2021-09-13 20:57:18 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQToObjectsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names =
+            {
+                 "Life is Beautiful",
+                "Arshika Agarwal",
+                "Seven Pounds",
+                "Rupali Agarwal",
+                "Pearl Solutions",
+                "Vamika Agarwal",
+                "Vidya Vrat Agarwal",
+                "C-Sharp Corner Mumbai Chapter"
+            };
+            //Linq query  
+            IEnumerable<string> namesOfPeople = from name in names
+                                                where name.Length <= 16
+                                                select name;
+            //OR
+            /*
+            var namesOfPeople = from name in names
+                        where name.Length <= 16
+                        select name;
+            */
+            foreach (var name in namesOfPeople)
+            {
+                Console.WriteLine(name + "\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
